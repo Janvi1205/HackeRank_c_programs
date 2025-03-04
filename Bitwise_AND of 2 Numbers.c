85a43e7cf868b966a308a422e36c466c3ffc8b7f
Janvi Yadav <janviyadav120505@gmail.com>
2025-03-04 18:56:47 +0530

    Added Bitwise_AND of 2 Numbers

---
+++ Bitwise_AND of 2 Numbers.c
@@ -0,0 +1,33 @@
+/*Write a program to find the bitwise AND of two given integers a and b without using the & operator. You can use other bitwise operators like |, ^, ~, and shifts to compute the result.
+
+Input Format
+
+A single line containing two integers a and b.
+Constraints
+
+-10^5 ≤ a,b ≤ 10^5
+Output Format
+
+Print the result of a&b computed without using the & operator.
+Sample Input 0
+
+5 3
+Sample Output 0
+
+1*/
+
+#include <stdio.h>
+
+int main() {
+    
+     int a, b;
+
+    scanf("%d %d", &a, &b);
+
+    int result = ~(~a | ~b);
+
+    printf("%d\n", result);
+
+
+    return 0;
+}
